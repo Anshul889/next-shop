cc3b20b5457e3927b0a887401c6bf4ce657f6c8f
anshulmanaktala@gmail.com <anshulmanaktala@gmail.com>
2022-07-24 17:49:57 +0530

    firestore next auth adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth/next'
+import GoogleProvider from 'next-auth/providers/google'
+import { FirestoreAdapter } from "@next-auth/firebase-adapter"
 
 export default NextAuth({
   providers: [
@@ -9,13 +10,21 @@ export default NextAuth({
     }),
   ],
   pages: {
-    signIn: "/auth/signin",
+    signIn: '/auth/signin',
   },
   secret: process.env.NEXT_AUTH_SECRET,
   callbacks: {
-    async session({session, token, user}){
-      session.user.uid = token.sub;
+    async session({ session, token, user }) {
       return session
-    }
+    },
   },
-});
+    adapter: FirestoreAdapter({
+    apiKey: process.env.FIREBASE_API_KEY,
+    appId: process.env.FIREBASE_APP_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  }),
+})
