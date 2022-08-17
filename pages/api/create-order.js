23a552a15321b7217a8945842fe13b8d9c686a39
anshulmanaktala@gmail.com <anshulmanaktala@gmail.com>
2022-08-17 14:39:19 +0530

    admin private key

--- pages/api/create-order.js
+++ pages/api/create-order.js
@@ -5,7 +5,6 @@ async function CreateOrder(req, res) {
   const { session_id } = req.body
   const session = await stripe.checkout.sessions.retrieve(session_id)
   const items = await stripe.checkout.sessions.listLineItems(session_id)
-  console.log('items:', items)
   if (session.payment_status === 'paid') {
     const doc = await db
       .collection('orders')
