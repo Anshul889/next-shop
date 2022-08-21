2ddd5ad712128d51ee57c03c7c191753cca9fff5
anshulmanaktala@gmail.com <anshulmanaktala@gmail.com>
2022-08-21 13:11:36 +0530

    fixed star rating to 5

--- components/StarRating/StarRating.jsx
+++ components/StarRating/StarRating.jsx
@@ -4,20 +4,22 @@ import starlight from '../../images/star-light.svg'
 import starsolid from '../../images/star-solid.svg'
 import { RatingContainer } from './style'
 
-const StarRating = ({rating, setRating}) => {
+const StarRating = ({ rating, setRating }) => {
   const [hover, setHover] = useState(0)
   return (
     <RatingContainer>
-      {[...Array(5)].map((star, index) => {
+      {[1, 2, 3, 4, 5].map((star, index) => {
         index += 1
-        return index <= ((rating && hover) || hover)  ? (
+        return index <= ((rating && hover) || hover) ? (
           <div
             type="button"
             key={index}
             onClick={() => setRating(index)}
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(rating)}
-          ><Image src={starsolid} height={25} width={25} alt=''/></div>
+          >
+            <Image src={starsolid} height={25} width={25} alt="" />
+          </div>
         ) : (
           <div
             type="button"
@@ -25,7 +27,9 @@ const StarRating = ({rating, setRating}) => {
             onClick={() => setRating(index)}
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(rating)}
-          ><Image src={starlight} alt='' height={25} width={25}/></div>
+          >
+            <Image src={starlight} alt="" height={25} width={25} />
+          </div>
         )
       })}
     </RatingContainer>
