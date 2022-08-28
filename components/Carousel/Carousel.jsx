baa66beae6916094c3243ed1572dd40e69880533
anshulmanaktala@gmail.com <anshulmanaktala@gmail.com>
2022-08-28 14:15:27 +0530

    image optimization

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -6,8 +6,8 @@ const Carousel = ({ url1, url2, url3, url4 }) => {
   return (
     <Wrapper>
       <Container>
-        <Image src={url1} width="100%" height="100%" layout="responsive" objectFit="cover" alt=""/>
-        <Image src={url2} width="100%" height="100%" layout="responsive" objectFit="cover" alt=""/>
+        <Image src={url1} width="350" height="350" layout="responsive" objectFit="cover" alt=""/>
+        <Image src={url2} width="350" height="350" layout="responsive" objectFit="cover" alt=""/>
         <Image src={url3} width="100%" height="100%" layout="responsive" objectFit="cover" alt=""/>
         <Image src={url4} width="100%" height="100%" layout="responsive" objectFit="cover" alt=""/>
       </Container>
