f0bacdb49ad3897e38ce78758184947a17c7117e
anshulmanaktala@gmail.com <anshulmanaktala@gmail.com>
2022-08-28 15:28:25 +0530

    final img opt

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -6,17 +6,24 @@ const Carousel = ({ url1, url2, url3, url4 }) => {
   return (
     <Wrapper>
       <Container>
-        <div style={{display: 'block', position:'relative'}}>
-        <Image src={url1} width="350" height="350"  alt="" layout='fill' priority/>
+        <div style={{ display: 'block', position: 'relative' }}>
+          <Image
+            src={url1}
+            width="350"
+            height="350"
+            alt=""
+            layout="fill"
+            priority
+          />
         </div>
-        <div style={{display: 'block', position:'relative'}}>
-        <Image src={url2} width="350" height="350" layout="responsive" objectFit="cover" alt=""/>
+        <div style={{ display: 'block', position: 'relative' }}>
+          <Image src={url2} width="350" height="350" layout="fill" alt="" />
         </div>
-        <div style={{display: 'block', position:'relative'}}>
-        <Image src={url3} width="350" height="350" layout="responsive" objectFit="cover" alt=""/>
+        <div style={{ display: 'block', position: 'relative' }}>
+          <Image src={url3} width="350" height="350" layout="fill" alt="" />
         </div>
-        <div style={{display: 'block', position:'relative'}}>
-        <Image src={url4} width="350" height="350" layout="responsive" objectFit="cover" alt=""/>
+        <div style={{ display: 'block', position: 'relative' }}>
+          <Image src={url4} width="350" height="350" layout="fill" alt="" />
         </div>
       </Container>
     </Wrapper>
