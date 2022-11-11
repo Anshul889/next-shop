9406ed82ba12f414b9ff3c77a0566920f2b3260b
anshulmanaktala@gmail.com <anshulmanaktala@gmail.com>
2022-11-11 13:47:52 +0530

    padding fix products

--- styles/products.js
+++ styles/products.js
@@ -1,30 +1,32 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  padding-bottom: 25px;
+`
 
 export const Heading = styled.div`
-width: 90%;
-margin: 15px 0 15px 5%;
-@media screen and (min-width: 768px) {
-  width: 60%;
-  h1{
-    font-size: 48px;
+  width: 90%;
+  margin: 15px 0 15px 5%;
+  @media screen and (min-width: 768px) {
+    width: 60%;
+    h1 {
+      font-size: 48px;
+    }
+    p {
+      font-size: 28.8px;
+      font-weight: 400;
+    }
   }
-  p{
-    font-size: 28.8px;
-    font-weight: 400;
-  }
-}
 `
 
 export const Filters = styled.div`
-display: grid;
-grid-template-columns: repeat(2, 1fr);
-width: 90%;
-margin: 0 auto;
-@media screen and (min-width: 768px) {
-  grid-template-columns: repeat(5, 1fr);
-}
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 90%;
+  margin: 0 auto;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
 `
 
 export const Container = styled.div`
@@ -37,4 +39,3 @@ export const Container = styled.div`
     column-gap: 25px;
   }
 `
-
