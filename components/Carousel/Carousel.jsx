4752a5fcfc93e277e6c328b6031aa208ade28942
anshulmanaktala@gmail.com <anshulmanaktala@gmail.com>
2022-12-19 18:29:06 +0530

    iphone ssr carousel

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -7,13 +7,13 @@ const Carousel = ({ url1, url2, url3, url4 }) => {
     <Wrapper>
       <Container>
         <div style={{display: 'block', position:'relative'}}>
-        <Image src={url1} width="350" height="350"  alt="" layout='fill' priority/>
+        <Image src={url1} width="350" height="350"  alt="" layout='responsive' priority/>
         </div>
         <div style={{display: 'block', position:'relative'}}>
-        <Image src={url2} width="350" height="350" layout="fill" alt=""/>
+        <Image src={url2} width="350" height="350" layout="responsive" alt=""/>
         </div>
         <div style={{display: 'block', position:'relative'}}>
-        <Image src={url3} width="350" height="350" layout="fill"  alt=""/>
+        <Image src={url3} width="350" height="350" layout="responsive"  alt=""/>
         </div>
         <div style={{display: 'block', position:'relative'}}>
         <Image src={url4} width="350" height="350" layout="responsive" alt=""/>
