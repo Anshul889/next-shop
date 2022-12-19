55cc9b22eca9cb5f480ac89cea3eb2b70a700b3f
anshulmanaktala@gmail.com <anshulmanaktala@gmail.com>
2022-12-19 18:36:07 +0530

    carousel fix

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -6,17 +6,17 @@ const Carousel = ({ url1, url2, url3, url4 }) => {
   return (
     <Wrapper>
       <Container>
-        <div style={{display: 'block', position:'relative'}}>
-        <Image src={url1} width="350" height="350"  alt="" layout='responsive' priority/>
+        <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+          <Image src={url1} alt="" layout="fill" priority />
         </div>
-        <div style={{display: 'block', position:'relative'}}>
-        <Image src={url2} width="350" height="350" layout="responsive" alt=""/>
+        <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+          <Image src={url2} layout="fill" alt="" />
         </div>
-        <div style={{display: 'block', position:'relative'}}>
-        <Image src={url3} width="350" height="350" layout="responsive"  alt=""/>
+        <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+          <Image src={url3} layout="fill" alt="" />
         </div>
-        <div style={{display: 'block', position:'relative'}}>
-        <Image src={url4} width="350" height="350" layout="responsive" alt=""/>
+        <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+          <Image src={url4} layout="fill" alt="" />
         </div>
       </Container>
     </Wrapper>
